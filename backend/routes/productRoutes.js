4710764e219481444ce8cc82f9072e31be025ab3
radu-bordea <radu.bordea.dev@gmail.com>
2024-05-22 15:08:53 +0300

    create error handlers

--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -7,6 +7,7 @@ router.get(
   "/",
   asyncHandler(async (req, res) => {
     const products = await Product.find({});
+    throw new Error('Some error')
     res.json(products);
   })
 );
@@ -18,7 +19,8 @@ router.get(
     if (product) {
       return res.json(product);
     }
-    res.status(404).json({message: "Product not found"})
+    res.status(404);
+    throw new Error("Resource not found");
   })
 );
 
