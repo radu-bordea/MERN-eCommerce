8536959ee93f9e9739d1c2b4354405e23c3251b7
radu-bordea <radu.bordea.dev@gmail.com>
2024-06-03 17:55:20 +0200

    add item count in the Header component

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,9 +1,13 @@
-import { Navbar, Nav, Container } from "react-bootstrap";
+import { Badge, Navbar, Nav, Container } from "react-bootstrap";
 import { FaShoppingCart, FaUser } from "react-icons/fa";
 import { LinkContainer } from "react-router-bootstrap";
 import logoOliwia from "../assets/logoOliwia.jpg";
+import { useSelector } from "react-redux";
 
 const Header = () => {
+  const { cartItems } = useSelector((state) => state.cart);
+  
+
   return (
     <div>
       <Navbar bg="dark" variant="dark" expand="md" collapseOnSelect>
@@ -26,6 +30,13 @@ const Header = () => {
               <LinkContainer to="/cart">
                 <Nav.Link>
                   <FaShoppingCart /> Cart
+                  {
+                    cartItems.length > 0 && (
+                      <Badge pill bg="success" style={{marginLeft: '5px'}}>
+                        {cartItems.reduce((a, c)=> a + c.qty, 0 )}
+                      </Badge>
+                    )
+                  }
                 </Nav.Link>
               </LinkContainer>
               <LinkContainer to="/login">
