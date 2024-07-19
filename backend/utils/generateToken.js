6f60e1f15e9411ee7601d6b7d07544978092384a
radu-bordea <radu.bordea.dev@gmail.com>
2024-07-19 21:10:32 +0200

    fix paypal

--- backend/utils/generateToken.js
+++ backend/utils/generateToken.js
@@ -1,18 +1,16 @@
-import jwt from "jsonwebtoken";
+import jwt from 'jsonwebtoken';
 
-// Function to generate a JWT token and set it as a cookie in the response
 const generateToken = (res, userId) => {
-  // Create a JWT token with the userId as the payload, using the secret key from environment variables
   const token = jwt.sign({ userId }, process.env.JWT_SECRET, {
-    expiresIn: "30d",  // Token expires in 30 days
+    expiresIn: '30d',
   });
 
-  // Set the JWT token as an HTTP-only cookie in the response
-  res.cookie("jwt", token, {
-    httpOnly: true,  // Cookie is only accessible via HTTP(S), not JavaScript
-    secure: process.env.NODE_ENV !== "development",  // Cookie is only sent over HTTPS in production
-    sameSite: "strict",  // Cookie is only sent for same-site requests to prevent CSRF attacks
-    maxAge: 30 * 24 * 60 * 60 * 1000,  // Cookie expires in 30 days (in milliseconds)
+  // Set JWT as an HTTP-Only cookie
+  res.cookie('jwt', token, {
+    httpOnly: true,
+    secure: process.env.NODE_ENV !== 'development', // Use secure cookies in production
+    sameSite: 'strict', // Prevent CSRF attacks
+    maxAge: 30 * 24 * 60 * 60 * 1000, // 30 days
   });
 };
 
